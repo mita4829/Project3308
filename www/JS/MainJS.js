3195bbacd3ed8b5be77231d3fbd4fff4778802d7
mita4829 <mita4829@colorado.edu>
2016-02-19 07:52:16 -0700

    Update css to check if mobile

--- www/JS/MainJS.js
+++ www/JS/MainJS.js
@@ -1,6 +1,6 @@
 //Main javascript for home page
 function onload(){ //website calls this function when the website first loads up
-    
+    checkIfMobile();
 }
 
 //firstEvents and secondEvents functions are here just to keep things organized. Use them if you like, or use it to call other functions you've made
@@ -19,4 +19,11 @@ function lastEvents(){
 function searchForBook(userInput){ /* userInput is the name of the book the user is searching for. */
     /* Add more code here to connect with the SQL server to get the location of the book */
     alert(userInput);
+}
+
+function checkIfMobile(){
+    var mobile = (/Android|webOS|iPhone|iPad|iPod|Windows Phone|Kindle|IEMobile/i.test(navigator.userAgent)); //return boolean: Check to see if it's a mobile device, if false, disable site.
+    if(!mobile){
+        document.getElementById('userFormToSearchBooks').style.display = 'none';
+    }
 }
