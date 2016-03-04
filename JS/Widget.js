7445b9af577546514acaa1a15e91fad8cbb836f5
mita4829 <mita4829@colorado.edu>
2016-03-04 15:39:52 -0700

    Remove demo code

--- JS/Widget.js
+++ JS/Widget.js
@@ -5,24 +5,12 @@ function requestXYZ(){
             var y = Math.round(event.beta);
             var z = Math.round(event.alpha);
             if(x >= 20){
-                document.getElementById('pano').style.left = (10*z)-3600+"px";//All panos need a constant to prevent the jumping effect when viewing panos.
+                document.getElementById('pano').style.left = (-3600+10*z)+"px";//All panos need a constant to prevent the jumping effect when viewing panos.
+                document.getElementById('debugPos').innerHTML = (-3600+10*z)+" "+z;
             }else if(x <= -20){
-                document.getElementById('pano').style.left = 10*(z+180)-7200+"px";
+                document.getElementById('pano').style.left = 10*(z+180)-1800+"px";
             
             }
         }, true);
     
-}
-//hmmm a global array for now...
-var places = [12,11,10,7,5,3,1,0];
-var i = 0;
-function demo(){
-    //tmp function
-    alert();
-    if(places[i] != 0){
-    document.getElementById('pano').src = "Panos/"+places[i]+".jpg";
-    i++;
-    }else{
-        alert("You have arrived!");
-    }
 }
