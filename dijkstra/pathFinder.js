b9396d4f436af8660ba9e2b01805da736a536c47
Owen <owattenmaker@gmail.com>
2016-04-14 21:57:34 -0600

    updated graph with new nodes

--- dijkstra/pathFinder.js
+++ dijkstra/pathFinder.js
@@ -124,127 +124,121 @@ function nSearch() {
     // now hard coded in so it is easier to access the graph. If you want the graph in another arrangement,
     // let me know or do it yourself :)
     this.addVertex('1', {
-	2: 1,
-	3: 1
-    });
+        2: 1,
+        });
     this.addVertex('2', {
-	1: 1,
-	4: 1
+	3: 1,
     });
     this.addVertex('3', {
-	1: 1,
-	5: 1,
+	4: 1,
+	7: 1,
+    12: 1,
     });
     this.addVertex('4', {
-	2: 1,
 	5: 1,
-	6: 1,
-	7: 2,
+    6: 1,
+	7: 1,
     });
     this.addVertex('5', {
-	3: 1,
 	4: 1,
-	6: 1,
-	7: 1
     });
     this.addVertex('6', {
 	4: 1,
-	5: 2,
 	7: 1
     });
     this.addVertex('7', {
-	4: 2,
-	5: 1,
+	3: 1,
+    4: 1,
 	6: 1,
-	10: 1,
-	18: 1
     });
     this.addVertex('8', {
 	9: 1,
-	27: 1
+	14: 1
     });
     this.addVertex('9', {
-	8: 1
+	8: 1,
+    10: 1,
     });
     this.addVertex('10', {
-	7: 1,
 	11: 1,
-	27: 1
+	13: 1
     });
     this.addVertex('11', {
 	10: 1,
 	12: 1
     });
     this.addVertex('12', {
-	11: 1
+	3: 1,
+    11: 1,
+    13: 1
     });
     this.addVertex('13', {
-	14: 1,
-	16: 1
+	10: 1,
+	14: 1
     });
     this.addVertex('14', {
-	13: 1,
-	15: 1
+	8: 1,
+	13: 1
     });
     this.addVertex('15', {
-	14: 1,
-	16: 1
-    });
-    this.addVertex('16', {
-	13: 1,
-	15: 1,
-	17: 1,
-	18: 1
-    });
-    this.addVertex('17', {
-	16: 1
-    });
-    this.addVertex('18', {
-	7: 1,
-	15: 1,
 	16: 1,
 	20: 1
     });
+    this.addVertex('16', {
+	15: 1,
+	17: 1,
+    });
+    this.addVertex('17', {
+        16: 1,
+        18: 1,
+        19: 1,
+        20: 1
+    });
+    this.addVertex('18', {
+	17: 1,
+	19: 1,
+    });
     this.addVertex('19', {
-	27: 1
+        17: 1,
+        18: 1,
+        20: 1,
+        12: 1,
     });
     this.addVertex('20', {
 	15: 1,
-	18: 1,
-	21: 1
+	17: 1,
+	19: 1,
+        21: 1
     });
     this.addVertex('21', {
 	20: 1,
-	22: 1,
-	23: 1
     });
     this.addVertex('22', {
-	21: 1,
-	23: 1
+	23: 1,
+	24: 1
     });
     this.addVertex('23', {
-        21: 1,
         22: 1,
-        24: 1
+        24: 1,
+        27: 1
     });
     this.addVertex('24', {
-        23: 1,
-        25: 1
+        18: 1,
+        22: 1,
+        23: 1
     });
     this.addVertex('25', {
-        24: 1,
-        26: 1
+        26: 1,
+        27: 1
     });
     this.addVertex('26', {
-        25: 1
+        25: 1,
+        27: 1
     });
     this.addVertex('27', {
-        8: 1,
-        10: 1,
-        19: 1
+        23: 1,
+        25: 1,
+        26: 1
     });
 }
 
-
-// the fun part. I know something is probably messed up here, but oh well
-var n = new nSearch();
