42d48c03a9dd963ccea412d958a8d0d27ca9063e
mita4829 <mita4829@colorado.edu>
2016-04-19 20:24:59 -0600

    something broke

--- JS/Widget.js
+++ JS/Widget.js
@@ -66,7 +66,8 @@ function beginDataParse(data){
     if(data){
         path = n.dijkstra(start.toString(),endlocation.toString());
         alert(endlocation);
-        document.write(path);
+        alert("New");
+        document.write(n.dijkstra("1","25");
         document.getElementById('pano').src = "PanoArrows/"+path[0]+"_"+path[1]+".jpg";//Beta 1.0 before arrows
     }
 }
